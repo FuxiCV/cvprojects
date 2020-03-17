0b595e42a135c9608ef3f995441d3e886d3a5721
yiyuan1991 <yyshawn@163.com>
2020-03-17 14:28:15 +0800

    v5

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  title: "FuxiCV",
+  description: "The description of the site.",
+  head: [["link", { rel: "icon", href: `/icons/favicon.ico` }]],
+  base: "/fuxicv/",
+  dest: "./dist",
+
+  themeConfig: {
+    sidebarDepth: 2,
+    search: false,
+    sidebar: [' '],
+    // displayAllHeaders: true, // 默认值：false
+    /*
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "About", link: "/about/" },
+      { text: "Projects", link: "/projects/" },
+      { text: "Guide", link: "/guide/" },
+      { text: "GitHub", link: "https://github.com/yiyuan1991" }
+    ],
+    sidebar: {
+      '/guide/': genSidebarConfig('Guide')
+    },
+    */
+    lastUpdated: 'Last Updated'
+  },
+
+  markdown: {
+    // options for markdown-it-anchor
+    anchor: { permalink: false },
+    extendMarkdown: md => {
+      md.use(require("markdown-it-katex"));
+    }
+  }
+};
+
+function genSidebarConfig (title) {
+  return [
+    {
+      title,
+      collapsable: false,
+      children: [
+        '',
+        'getting-started',
+        'customize',
+        'advanced',
+      ]
+    }
+  ]
+}
+
